6272e0e40685e4fc0a93ae8ae52f62b7515ff078
Andrii Stolitnii <andrii.stolitnii01@gmail.com>
2024-06-07 22:26:03 +0200

    View fix

--- SQL/views.sql
+++ SQL/views.sql
@@ -5,10 +5,10 @@ CREATE VIEW educational_certificates_view AS
 SELECT
     id,
     (
-        SELECT name FROM educational_certificates_types WHERE id = type
+        SELECT name FROM educational_certificates_types WHERE id = edu_cert.kind
     ) AS "Level of education",
     (
-        SELECT name FROM educational_instances WHERE id = issuer
+        SELECT name FROM educational_instances WHERE id = edu_cert.issuer
     ) AS "Issuer instance",
     issue_date AS "Date of issue"
-FROM educational_certificates;
+FROM educational_certificates edu_cert ORDER BY issue_date DESC;
